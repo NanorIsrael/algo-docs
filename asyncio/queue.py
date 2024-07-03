b5611040f2092479d6d9fc3cc9dd9885f0d887c7
nanorisrael <israel.nanor@compozelabs.com>
2024-07-03 16:21:49 +0000

    Add python asyncio example

---
+++ asyncio/queue.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# asyncq.py
+import itertools as it
+import asyncio
+import random
+import time
+import os
+
+async def makeitem(size: int = 5) -> str:
+	return os.urandom(size).hex()
+
+async def randsleep(caller = None) -> None:
+	i = random.randint(0, 10)
+	if caller:
+		print(f"{caller} sleeping for {i} seconds.")
+		await asyncio.sleep(i)
+
+async def produce(name: int, q: asyncio.Queue) -> None:
+	n = random.randint(0, 10)
+	for _ in it.repeat(None, n):
+		await randsleep(caller=f"Producer {name}")
+		i = await makeitem()
+		t = time.perf_counter()
+		await q.put((i, t))
+		print(f'Prodcuer {name} added <{i}> to queue')
+
+async def consume(name: int, q: asyncio.Queue) -> None:
+	while True:
+		await randsleep(caller=f"consumer {name}")
+		i, t = await q.get()
+		now = time.perf_counter()
+		print(f"Consumer {name} got element <{i}>"
+              f" in {now-t:0.5f} seconds.")
+		q.task_done()
+
+async def main(nprod: int, ncon: int):
+	q = asyncio.Queue()
+	producers = [asyncio.create_task(produce(n, q)) for n in range(nprod)]
+	consumers = [asyncio.create_task(consume(n, q)) for n in range(ncon)]
+	await asyncio.gather(*producers)
+	await q.join()
+	for c in consumers:
+		c.cancel()
+
+
+
+if __name__ == '__main__':
+	start = time.perf_counter()
+	# print(makeitem())
+	asyncio.run(main(10, 10))
+	elapsed = time.perf_counter() - start
+	print(f"Program completed in {elapsed:0.5f} seconds.")
+
