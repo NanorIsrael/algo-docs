ae332746c8f64b407458a11645843cb76cdc47ea
nanorisrael <israel.nanor@compozelabs.com>
2024-07-04 10:52:08 +0000

    Update scrapper to handle bulk writes

--- asyncio/scrapper.py
+++ asyncio/scrapper.py
@@ -4,10 +4,13 @@ import asyncio
 import logging
 import urllib.error
 import urllib.parse
+from typing import IO
 import re
 import sys
 
-from aiohttp import ClientSession, ClientError
+import aiofiles
+import aiohttp
+
 
 logging.basicConfig(
 	format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
@@ -21,7 +24,7 @@ logging.getLogger('chardet.charsetprober').disabled
 HREF_RE = re.compile(r'href="(.*?)"')
 # **kwargs
 async def fetch_html(url) -> str:
-	async with ClientSession() as session:
+	async with aiohttp.ClientSession() as session:
 			async with session.get(url) as resp:
 				resp.raise_for_status()
 				logger.info("Got response [%s] for URL: %s", resp.status, url)
@@ -29,7 +32,7 @@ async def fetch_html(url) -> str:
 				return html
 	   
 
-async def parse(url: str):
+async def parse(url: str) -> set:
 	found = set()
 	try:
 		html = await fetch_html(url)
@@ -59,7 +62,39 @@ async def parse(url: str):
 			)
 			return found
 
+async def write_one(file: IO, url: str) -> None:
+	"""Write the found HREFs from `url` to `file`."""
+	resp = await parse(url)
+	if not resp:
+		return None
+	async with aiofiles.open(file, 'a') as f:
+		for p in resp:
+			await f.write(f"{url}\t{p}")
+		logger.info("Wrote results for source URL: %s", url)
+
+async def bulk_crawl_write(file: IO, urls: set) -> None:
+	"""Crawl & write concurrently to `file` for multiple `urls`."""
+	task = []
+	for url in urls:
+		task.append(
+			write_one(file, url)
+		)
+	await asyncio.gather(*task)
+
 
 if __name__ == '__main__':
-	asyncio.run(parse("https://regex101.com/"))
+	import pathlib
+	import sys
+
+	assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
+	parentdir = pathlib.Path(__file__).parent
+	with open(parentdir.joinpath("urls.txt")) as infile:
+		urls = set(map(str.strip, infile))
+
+	outpath = parentdir.joinpath('foundurls.txt')
+	with open(outpath, 'w') as outfile:
+		outfile.write("source_url\tparsed_url\n")
+
+	print(parentdir)
+	asyncio.run(bulk_crawl_write(outpath, urls))
 
